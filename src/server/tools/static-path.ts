c1a19b3b04b464d603215f1fa19a1c956fb3078e
Phineas Ziegler <phinziegler@gmail.com>
2023-03-24 14:37:47 -0400

    fix static path

--- src/server/tools/static-path.ts
+++ src/server/tools/static-path.ts
@@ -7,5 +7,5 @@ require('dotenv').config();
     The ternary operator is used to change the path... this is because the app runs in a different location 
     when it has been built vs when running the typescript directly (which is the case for npm run dev)
 */
-const STATIC_PATH = process.env.NODE_ENV === Environments.PRODUCTION ? path.join(__dirname, "../../client") : path.join(__dirname, "../../builds/client");
+const STATIC_PATH = process.env.NODE_ENV === Environments.PRODUCTION ? path.join(__dirname, "../../../client") : path.join(__dirname, "../../../builds/client");
 export default STATIC_PATH;
