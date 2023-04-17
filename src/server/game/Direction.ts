5997cf6ea251bafd839dc6c118a540db78cffc35
Aracelli Doescher <45574970+sillycelli@users.noreply.github.com>
2023-04-16 23:12:38 -0400

    create bulk of game architecture

--- src/server/game/Direction.ts
+++
@@ -1,7 +0,0 @@
-enum Direction {
-    LEFT = 0,
-    RIGHT = 1,
-    FORWARD = 2,
-    BACK = 3
-}
-export default Direction;
--- src/shared/Room.ts
+++ src/shared/Room.ts
@@ -6,19 +6,4 @@ export default interface Room {
     isSafe: boolean;
     isGoal: boolean;
     isVisited: boolean;
-
-    // constructor(forward: Boolean = false, right: Boolean = false, back: Boolean = false, 
-    //     left: Boolean = false, isSafe: Boolean = true, isGoal: Boolean = false) {
-    //     this.up = forward;
-    //     this.right = right;
-    //     this.down = back;
-    //     this.left = left;
-    //     this.isSafe = isSafe;
-    //     this.isGoal = isGoal;
-    //     this.isVisited = false;
-    // }
-
-    // public toString(): string {
-    //     return "{Left: " + this.left?.toString() + "\nRight: \n" + this.right?.toString() + "\nForward: \n" + this.forward?.toString() + "\nBack: \n" + this.back?.toString() + "}";
-    // }
 }
